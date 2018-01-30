fa9c95f51cc4d52e2ddebcf70fedb2a369da18dd
sequeirajonathan <32176675+sequeirajonathan@users.noreply.github.com>
2018-01-30 16:35:45 -0500

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+class Main {
+  
+  public static void main (String[] args) {
+    HashMap<String,Boolean> myBooks =  new HashMap<String,Boolean>();
+    
+    myBooks.put("Road Down The Funnel", true);
+    myBooks.put("Rat: A Biology", false);
+    myBooks.put("TimeIn", true);
+    myBooks.put("3D Food Printing", false);
+    
+    Library myLibrary = new Library();
+    
+    myLibrary.getFinishedBooks(myBooks);
+    myLibrary.getUnfinishedBooks(myBooks);
+  }
+  
+  public Library (){
+    
+  }
+  
+  public static void getFinishedBooks(HashMap <String, Boolean> library){
+    if(library.size() < 1) {
+      System.out.println("HashMap is empty");
+    } else {
+      for (String book : library.keySet()) {
+        	if(library.get(book) == true){
+        		System.out.println("Finished Book: " + book);
+        }
+      }
+    }
+  }
+  
+  public static void getUnfinishedBooks(HashMap <String, Boolean> library){
+    if(library.size() < 1) {
+      System.out.println("HashMap is empty");
+    } else {
+      for (String book : library.keySet()) {
+        	if(library.get(book) == false){
+        		System.out.println("Incomplete Book: " + book);
+        }
+      }
+    }
+  }
+}
